13d63cd372936cfd28dbe6ad67273990a6f41c8d
Cristiano <cs.almeida@protonmail.ch>
2023-09-09 11:58:16 +0700

    Chapter 1 exercises.

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -4,13 +4,20 @@
 // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
 // Execute `rustlings hint if2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn foo_if_fizz(fizzish: &str) -> &str {
-    if fizzish == "fizz" {
-        "foo"
-    } else {
-        1
+    // if fizzish == "fizz" {
+    //     "foo"
+    // } else if fizzish == "fuzz" {
+    //     "bar"
+    // } else {
+    //     "baz"
+    // }
+
+    // A match expression solves the issue the same in this problem.
+    match fizzish {
+        "fizz" => "foo",
+        "fuzz" => "bar",
+        _ => "baz",
     }
 }
 
